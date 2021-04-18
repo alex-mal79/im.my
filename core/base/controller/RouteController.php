34b7bd47b5ef8c107881757bb3ab2e4d09ef7d33
alex_mal79 <a-79maleev@yandex.ru>
2021-04-18 20:56:19 +0300

    creating: RouteController, Singlrton pattern

--- core/base/controller/RouteController.php
+++ core/base/controller/RouteController.php
@@ -1,7 +1,20 @@
 <?php
 namespace core\base\controller;
 
-
+//Основной маршруный контроллер
 class RouteController{
+    static private $_instance;
 
+    private function __clone(){
+    }
+
+    static public function getInstance(){
+        if(self::$_instance instanceof self){
+            return self::$_instance;
+        }
+        return self::$_instance = new self();
+    }
+
+    private function __construct(){
+    }
 }

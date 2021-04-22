919d5dd39a748fc777009df26c3c138e03b5bffa
alex_mal79 <a-79maleev@yandex.ru>
2021-04-22 23:22:19 +0300

    finish routing

---
+++ core/admin/controller/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+namespace core\admin\controller;
+
+use core\base\controller\BaseController;
+
+class IndexController extends BaseController {
+    protected function inputData(){
+        exit('Админпанель');
+    }
+
+}
